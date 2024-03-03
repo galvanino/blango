5e7194e1341609d0d22bcbbb33f7fdc90a5e22a7
Moisés Niño <galvanino@gmail.com>
2024-03-03 00:11:48 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -46,6 +46,12 @@ class Dev(Configuration):
     ACCOUNT_ACTIVATION_DAYS = 7
     # REGISTRATION_OPEN = False
 
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+    SITE_ID = 1
     # Application definition
 
     INSTALLED_APPS = [
@@ -54,12 +60,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
         'blango_auth',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
 
     MIDDLEWARE = [
--- blango/urls.py
+++ blango/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
         name="django_registration_register",
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("accounts/", include("allauth.urls")),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
