c4fabeceb1248257fead9c7000693f10f875475e
Moisés Niño <galvanino@gmail.com>
2024-03-08 01:35:02 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -72,6 +72,7 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
+        'rest_framework.authtoken',
     ]
 
     MIDDLEWARE = [
@@ -84,6 +85,14 @@ class Dev(Configuration):
         'django.contrib.messages.middleware.MessageMiddleware',
     #    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
     INTERNAL_IPS = ['192.168.11.179']
 
